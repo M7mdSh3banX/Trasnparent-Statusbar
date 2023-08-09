2569f90c24f872802fee10cf441edeb6c06e1032
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-08-10 02:54:25 +0300

    remove unused imports

--- app/src/main/java/com/shaban/trasnparentstatusbar/MainActivity.kt
+++ app/src/main/java/com/shaban/trasnparentstatusbar/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.shaban.trasnparentstatusbar
 
-import android.content.res.Configuration
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
